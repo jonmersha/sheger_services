f23b4232dcc79e7a5a979e57e384069bb2a0f15c
yohannesm <jonmersha@gmail.com>
2024-07-29 17:15:12 +0300

    Minimization pass 2

--- src/modules/shop/get.js
+++ src/modules/shop/get.js
@@ -36,31 +36,6 @@ router.get("/count/:id", async (req, res) => {
   callFunc.getData(stm, res);
 });
 
-router.get("/counts/:id/:recId", async (req, res) => {
-  var data = {
-    "counts": {
-        "id": req.params.id,
-        "color": req.params.recId
-    }}
-    res.send(data)
-
-  // const id=req.params.id;
-  // const stm = selectOP.selectAllCount(data[id]);
-  // callFunc.getData(stm, res);
-});
-
-// router.get("/search/:id/:recId", async (req, res) => {
-//   var data = {
-//     "counts": {
-//         "id": req.params.id,
-//         "color": req.params.recId
-//     }}
-//     res.send(data)
-
-//   // const id=req.params.id;
-//   // const stm = selectOP.selectAllCount(data[id]);
-//   // callFunc.getData(stm, res);
-// });
 
 router.get("/search/:tableId/:recId", async (req, res) => {
   const tableId=req.params.tableId;
@@ -75,88 +50,88 @@ router.get("/search/:tableId/:recId", async (req, res) => {
 
 
 
-//Simple Response
-//1
-router.get('/category/get', (req, res) => {
-    const stm=selectOP.selectAll("users");
-   callFunc.addDataCallBack(stm,res);
-  });
-  //2
-  router.get('/category/get', (req, res) => {
-    const stm=selectOP.selectAll("category");
-   callFunc.addDataCallBack(stm,res);
-  });
-  router.get('/category/count/', (req, res) => {
-    const stm=selectOP.selectAllCount("category");
-   callFunc.addDataCallBack(stm,res);
-  });
+// //Simple Response
+// //1
+// router.get('/category/get', (req, res) => {
+//     const stm=selectOP.selectAll("users");
+//    callFunc.addDataCallBack(stm,res);
+//   });
+//   //2
+//   router.get('/category/get', (req, res) => {
+//     const stm=selectOP.selectAll("category");
+//    callFunc.addDataCallBack(stm,res);
+//   });
+//   router.get('/category/count/', (req, res) => {
+//     const stm=selectOP.selectAllCount("category");
+//    callFunc.addDataCallBack(stm,res);
+//   });
 
-  router.get('/category/count/:id', (req, res) => {
-    const id=req.params.id;
-    const stm = selectOP.selectCTRCount("category",id,"merchant_id");
-    callFunc.addDataCallBack(stm, res);
-  });
-  //3
-  router.get('/product/get', (req, res) => {
-    const stm=selectOP.selectAll("product");
-   callFunc.addDataCallBack(stm,res);
-  });
-  //Product Count
-  router.get('/product/count/get', (req, res) => {
-    const stm=selectOP.selectAllCount("product");
-   callFunc.addDataCallBack(stm,res);
-  });
-//Product Search by id
-  router.get("/product/:id", async (req, res) => {
-    const id=req.params.id;
-    const stm = selectOP.selectCTR("product",id,"id");
-    callFunc.addDataCallBack(stm, res);
-  });
+//   router.get('/category/count/:id', (req, res) => {
+//     const id=req.params.id;
+//     const stm = selectOP.selectCTRCount("category",id,"merchant_id");
+//     callFunc.addDataCallBack(stm, res);
+//   });
+//   //3
+//   router.get('/product/get', (req, res) => {
+//     const stm=selectOP.selectAll("product");
+//    callFunc.addDataCallBack(stm,res);
+//   });
+//   //Product Count
+//   router.get('/product/count/get', (req, res) => {
+//     const stm=selectOP.selectAllCount("product");
+//    callFunc.addDataCallBack(stm,res);
+//   });
+// //Product Search by id
+//   router.get("/product/:id", async (req, res) => {
+//     const id=req.params.id;
+//     const stm = selectOP.selectCTR("product",id,"id");
+//     callFunc.addDataCallBack(stm, res);
+//   });
 
-  //Product Search by categoty
-  router.get("/product/category/:id", async (req, res) => {
-    const id=req.params.id;
-    const stm = selectOP.selectCTR("product",id,"category");
-    callFunc.addDataCallBack(stm, res);
-  });
+//   //Product Search by categoty
+//   router.get("/product/category/:id", async (req, res) => {
+//     const id=req.params.id;
+//     const stm = selectOP.selectCTR("product",id,"category");
+//     callFunc.addDataCallBack(stm, res);
+//   });
 
-  //Product Count By Category 
-  router.get("/product/count/:id", async (req, res) => {
-    const id=req.params.id;
-    const stm = selectOP.selectCTRCount("product",id,"category");
-    callFunc.addDataCallBack(stm, res);
-  });
+//   //Product Count By Category 
+//   router.get("/product/count/:id", async (req, res) => {
+//     const id=req.params.id;
+//     const stm = selectOP.selectCTRCount("product",id,"category");
+//     callFunc.addDataCallBack(stm, res);
+//   });
   
-//4
-  router.get('/stock/get', (req, res) => {
-    const stm=selectOP.selectAll("stock_bin");
-   callFunc.addDataCallBack(stm,res);
-  });
+// //4
+//   router.get('/stock/get', (req, res) => {
+//     const stm=selectOP.selectAll("stock_bin");
+//    callFunc.addDataCallBack(stm,res);
+//   });
 
-  //5
-  router.get('/store/get', (req, res) => {
-    const stm=selectOP.selectAll("merchant_store");
-   callFunc.addDataCallBack(stm,res);
-  });
+//   //5
+//   router.get('/store/get', (req, res) => {
+//     const stm=selectOP.selectAll("merchant_store");
+//    callFunc.addDataCallBack(stm,res);
+//   });
 
-  //-->6
-  router.get('/order/get', (req, res) => {
-    const stm=selectOP.selectAll("product_order");
-   callFunc.addDataCallBack(stm,res);
-  });
-//-->7
-  router.get('/sells/get', (req, res) => {
-    const stm=selectOP.selectAll("product_selles");
-   callFunc.addDataCallBack(stm,res);
-  });
-//Complex Query 
-//1-->product of a given categoty
-//1-->
+//   //-->6
+//   router.get('/order/get', (req, res) => {
+//     const stm=selectOP.selectAll("product_order");
+//    callFunc.addDataCallBack(stm,res);
+//   });
+// //-->7
+//   router.get('/sells/get', (req, res) => {
+//     const stm=selectOP.selectAll("product_selles");
+//    callFunc.addDataCallBack(stm,res);
+//   });
+// //Complex Query 
+// //1-->product of a given categoty
+// //1-->
 
 
-router.get('/sells/get', (req, res) => {
-  const stm=selectOP.selectAll("product_selles");
- callFunc.addDataCallBack(stm,res);
-});
+// router.get('/sells/get', (req, res) => {
+//   const stm=selectOP.selectAll("product_selles");
+//  callFunc.addDataCallBack(stm,res);
+// });
 
 module.exports = router;
--- src/modules/shop/post.js
+++ src/modules/shop/post.js
@@ -27,47 +27,4 @@ router.post('/add/:id', (req, res) => {
   callFunc.addDataCallBack(stm,res,'ServiceCenter');
  });
 
-// //1-->User Registrations
-// router.post('/user/add', (req, res) => {
-//     const stm=selectOP.selectAll("users");
-//    callFunc.addDataCallBack(stm,res);
-//   });
-// //2-->product category Registrations
-//   router.post('/category/add', (req, res) => {
-//     let stm=insertOP.insert('category',req.body)
-//     callFunc.addDataCallBack(stm,res,'ServiceCenter');
-//   });
-
-// //-->Product Registrations
-//   router.post('/product/add', (req, res) => {
-//    let stm=insertOP.insert('product',req.body)
-//    callFunc.addDataCallBack(stm,res,'ServiceCenter');
-//   });
-
-//   router.post('/stock/add', (req, res) => {
-//    let stm=insertOP.insert('stock_bin',req.body)
-//    callFunc.addDataCallBack(stm,res,'ServiceCenter');
-//   });
-
-//   router.post('/store/add', (req, res) => {
-//    let stm=insertOP.insert('merchant_store',req.body)
-//    callFunc.addDataCallBack(stm,res,'ServiceCenter');
-
-//   });
-
-//   router.post('/order/add', (req, res) => {
-//    let stm=insertOP.insert('product_order',req.body)
-//    callFunc.addDataCallBack(stm,res,'ServiceCenter');
-//   });
-//   router.post('/sells/add', (req, res) => {
-//    let stm=insertOP.insert('product_selles',req.body)
-//    callFunc.addDataCallBack(stm,res,'ServiceCenter');
-//   });
-// router.post('/user/add', (req, res) => {
-//    let stm=insertOP.insert('users',req.body)
-//     callFunc.addDataCallBack(stm,res);
-//   });
-
-// //category
-
 module.exports = router;
