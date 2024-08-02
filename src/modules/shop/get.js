a8ddffcb173a1f7ec96f647a786c0b376b14283a
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 <jonmersha@gmail.com>
2024-08-02 17:26:43 +0300

    improved user search

--- src/modules/shop/get.js
+++ src/modules/shop/get.js
@@ -54,9 +54,9 @@ router.get("/search/:tableId/:recId", async (req, res) => {
 });
 
 //-----Get Users with Email ID
-router.get("/user/:email", async (req, res) => {
-  const email=req.params.email;
-  const stm = Query.selectCTRString('users',email,"user_email");
+router.get("/user/:uid", async (req, res) => {
+  const email=req.params.uid;
+  const stm = Query.selectCTRString('users',email,"uid");
   callFunc.getData(stm, res);
 });
 //--- Merchant Product BY category
@@ -71,91 +71,5 @@ router.get("/product/:val1/:val2", async (req, res) => {
 
 
 
-
-
-
-// //Simple Response
-// //1
-// router.get('/category/get', (req, res) => {
-//     const stm=selectOP.selectAll("users");
-//    callFunc.addDataCallBack(stm,res);
-//   });
-//   //2
-//   router.get('/category/get', (req, res) => {
-//     const stm=selectOP.selectAll("category");
-//    callFunc.addDataCallBack(stm,res);
-//   });
-//   router.get('/category/count/', (req, res) => {
-//     const stm=selectOP.selectAllCount("category");
-//    callFunc.addDataCallBack(stm,res);
-//   });
-
-//   router.get('/category/count/:id', (req, res) => {
-//     const id=req.params.id;
-//     const stm = selectOP.selectCTRCount("category",id,"merchant_id");
-//     callFunc.addDataCallBack(stm, res);
-//   });
-//   //3
-//   router.get('/product/get', (req, res) => {
-//     const stm=selectOP.selectAll("product");
-//    callFunc.addDataCallBack(stm,res);
-//   });
-//   //Product Count
-//   router.get('/product/count/get', (req, res) => {
-//     const stm=selectOP.selectAllCount("product");
-//    callFunc.addDataCallBack(stm,res);
-//   });
-// //Product Search by id
-//   router.get("/product/:id", async (req, res) => {
-//     const id=req.params.id;
-//     const stm = selectOP.selectCTR("product",id,"id");
-//     callFunc.addDataCallBack(stm, res);
-//   });
-
-//   //Product Search by categoty
-//   router.get("/product/category/:id", async (req, res) => {
-//     const id=req.params.id;
-//     const stm = selectOP.selectCTR("product",id,"category");
-//     callFunc.addDataCallBack(stm, res);
-//   });
-
-//   //Product Count By Category 
-//   router.get("/product/count/:id", async (req, res) => {
-//     const id=req.params.id;
-//     const stm = selectOP.selectCTRCount("product",id,"category");
-//     callFunc.addDataCallBack(stm, res);
-//   });
-  
-// //4
-//   router.get('/stock/get', (req, res) => {
-//     const stm=selectOP.selectAll("stock_bin");
-//    callFunc.addDataCallBack(stm,res);
-//   });
-
-//   //5
-//   router.get('/store/get', (req, res) => {
-//     const stm=selectOP.selectAll("merchant_store");
-//    callFunc.addDataCallBack(stm,res);
-//   });
-
-//   //-->6
-//   router.get('/order/get', (req, res) => {
-//     const stm=selectOP.selectAll("product_order");
-//    callFunc.addDataCallBack(stm,res);
-//   });
-// //-->7
-//   router.get('/sells/get', (req, res) => {
-//     const stm=selectOP.selectAll("product_selles");
-//    callFunc.addDataCallBack(stm,res);
-//   });
-// //Complex Query 
-// //1-->product of a given categoty
-// //1-->
-
-
-// router.get('/sells/get', (req, res) => {
-//   const stm=selectOP.selectAll("product_selles");
-//  callFunc.addDataCallBack(stm,res);
-// });
 
 module.exports = router;
