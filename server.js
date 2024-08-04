daa94e2ef9e387a80909aae60678e9fd298336c6
jonmersha@gmail.com <jonmersha@gmail.com>
2024-08-04 22:07:04 +0300

    image upload

--- server.js
+++ server.js
@@ -3,17 +3,14 @@ const app = express();
 var cors = require("cors");
 
 const port = 3000;
+
 app.use(cors());
 app.use(express.json());
+
 app.use("/static", express.static("public"));
 app.use("/movie", express.static("public/movie"));
 app.use("/shoping", express.static("public/shoping/images"));
 
-app.get("/test", (req, res) => {
-  res.send(
-    "This Is my task to test every thing again testing continued on the activityy"
-  );
-});
 ///Aduit System Services serivice is chaged well
 
 //============================================
@@ -60,6 +57,11 @@ app.use("/shaibuna", shaiBunnaService);
 //Remote Service Configurations
 app.listen();
 //Local service Configurations
+
+// app.post("/upd", upload.single("file"), (req, res) => {
+//   res.send("File uploaded successfully");
+// });
+
 // app.listen(port, () => {
 //   console.log(`Server Statrted @ ${port}`);
 // });
--- src/modules/shop/update.js
+++ src/modules/shop/update.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
 
-
-//Crud Opearions 
-const updateOP=require('../../utils/update')
+//Crud Opearions
+const updateOP = require("../../utils/update");
 
 //Call Back Functions
-const callFunc=require('./call_backs')
+const callFunc = require("./call_backs");
 
 /**
  * 0:'category',
@@ -16,16 +17,50 @@ const callFunc=require('./call_backs')
  * 4'stock_bin',
  * 6:'users'
  */
-let table=['category','merchant_store','product','product_order','stock_bin','users']
+let table = [
+  "category",
+  "merchant_store",
+  "product",
+  "product_order",
+  "stock_bin",
+  "users",
+];
 
-
-router.put('/update/:tableId',(req,res)=>{
-
-  const ID=req.params.tableId;
-  let stm= updateOP.update(table[ID],req.body);
+router.put("/update/:tableId", (req, res) => {
+  const ID = req.params.tableId;
+  let stm = updateOP.update(table[ID], req.body);
   console.log(stm);
-  callFunc.DBO(stm,res,'Error Updating');
-})
+  callFunc.DBO(stm, res, "Error Updating");
+});
 
+// Configure storage for multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/shoping/images"); // Set the destination to the public/uploads directory
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`); // Use timestamp to prevent file name collisions
+  },
+});
+
+// Initialize multer with the defined storage
+const upload = multer({ storage });
+
+//===============================================================================
+router.post("/upload/:tableId/:recId", upload.single("image"), (req, res) => {
+  const table = req.params.tableId;
+  const recID = req.params.recId;
+  let body = {
+    fields: {
+      image_name: req.file.filename,
+    },
+    criteria: {
+      id: recID,
+    },
+  };
+
+  let stm = updateOP.update(table, body);
+  callFunc.DBO(stm, res, "Error Uploading Images");
+});
 
 module.exports = router;
