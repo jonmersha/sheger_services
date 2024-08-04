4c97e5f5c133dc40cc82096a074e292389fe0c01
jonmersha@gmail.com <jonmersha@gmail.com>
2024-08-04 22:10:02 +0300

    git f

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use("/shopping", shopping_update);
 
 //==================================================
 
-///parking serivice
+///parking serivices
 const parkingService = require("./src/modules/parking/parking");
 app.use("/parking", parkingService);
 
