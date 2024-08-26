9f471e1d8cb2d9f2c035cce70e28ed98a4114f07
jonmersha@gmail.com <jonmersha@gmail.com>
2024-08-26 18:11:46 +0300

    more end added

--- src/modules/forex/get.js
+++ src/modules/forex/get.js
@@ -34,6 +34,7 @@ router.get("/data/:tableId", async (req, res) => {
   callFunc.DBO(stm, res, "Error Getting Data!!");
 });
 
+//Today Rate Only
 router.get("/rate/today", (req, res) => {
   const stm = `
   SELECT
@@ -62,84 +63,6 @@ WHERE
 });
 
 router.get("/rate/max", (req, res) => {
-  //   const stm = `
-  //   SELECT
-  //     rate.id AS rate_id,
-  //     rate.rate_date,
-  //     rate.buying_cash,
-  //     rate.buying_transaction,
-  //     rate.selling_cash,
-  //     rate.selling_transaction,
-  //     bank.id AS bank_id,
-  //     bank.short_name,
-  //     bank.bank_name,
-  //     bank.logo,
-  //     bank.color_main,
-  //     bank.color_back,
-  //     bank.color_text,
-  //     bank.email_address AS bank_email,
-  //     currency.id AS currency_id,
-  //     currency.name AS currency_name,
-  //     currency.description AS description,
-  //     currency.logo AS currency_logo
-  // FROM
-  //     rate
-  // JOIN
-  //     bank ON rate.bank_id = bank.id
-  // JOIN
-  //     currency ON rate.currency_id = currency.id
-  // JOIN (
-  //     SELECT
-  //         currency_id,
-  //         MAX(rate_date) AS max_rate_date,
-  //         MAX(buying_cash) AS max_buying_cash
-  //     FROM
-  //         rate
-  //     GROUP BY
-  //        currency_id
-  // ) AS LatestRates ON
-  // rate.currency_id = LatestRates.currency_id
-  // and  rate.buying_cash=max_buying_cash
-  //   // `;
-  //   SELECT
-  //     rate.id AS rate_id,
-  //     rate.rate_date,
-  //     rate.buying_cash,
-  //     rate.buying_transaction,
-  //     rate.selling_cash,
-  //     rate.selling_transaction,
-  //     bank.id AS bank_id,
-  //     bank.short_name,
-  //     bank.bank_name,
-  //     bank.logo,
-  //     bank.color_main,
-  //     bank.color_back,
-  //     bank.color_text,
-  //     bank.email_address AS bank_email,
-  //     currency.id AS currency_id,
-  //     currency.name AS currency_name,
-  //     currency.description AS description,
-  //     currency.logo AS currency_logo
-  // FROM
-  //     rate
-  // JOIN
-  //     bank ON rate.bank_id = bank.id
-  // JOIN
-  //     currency ON rate.currency_id = currency.id
-  // JOIN (
-  //     SELECT
-  //         currency_id,
-  //         MAX(rate_date) AS max_rate_date,
-  //         MAX(buying_cash) AS max_buying_cash
-  //     FROM
-  //         rate
-  //     GROUP BY
-  //        currency_id
-  // ) AS LatestRates ON
-  // rate.currency_id = LatestRates.currency_id
-  // AND rate.rate_date = LatestRates.max_rate_date
-  // ;
-  // `;
   const stm = `
   WITH latest_rates AS (
   SELECT 
@@ -195,12 +118,11 @@ FROM ranked_rates rr
 LEFT JOIN bank b ON b.id = rr.bank_id
 LEFT JOIN currency c ON rr.currency_id = c.id
 WHERE rr.rn = 1
-ORDER BY rr.currency_id;
-
-`;
+ORDER BY rr.currency_id;`;
 
   callFunc.DBO(stm, res, "Error Getting Data!!");
 });
+
 router.get("/rate/cbuying/max", (req, res) => {
   const stm = `
   WITH latest_rates AS (
@@ -263,7 +185,7 @@ ORDER BY rr.currency_id;
 
   callFunc.DBO(stm, res, "Error Getting Data!!");
 });
-router.get("/rate/cselling/min", () => {
+router.get("/rate/cselling/min", (req, res) => {
   const stm = `
   WITH latest_rates AS (
   SELECT 
@@ -325,7 +247,7 @@ ORDER BY rr.currency_id;
   callFunc.DBO(stm, res, "Error Getting Data!!");
 });
 
-router.get("/rate/trselling/min", () => {
+router.get("/rate/tselling/min", (req, res) => {
   const stm = `
   WITH latest_rates AS (
   SELECT 
@@ -386,7 +308,7 @@ ORDER BY rr.currency_id;
   callFunc.DBO(stm, res, "Error Getting Data!!");
 });
 
-router.get("/rate/trbuying/max", () => {
+router.get("/rate/tbuying/max", (req, res) => {
   const stm = `
   WITH latest_rates AS (
   SELECT 
@@ -497,6 +419,66 @@ select
  from rates
 left join bank on bank.id=rates.bank_id
 left join currency on rates.currency_id=currency.id`;
+  callFunc.DBO(stm, res, "Error Getting Data!!");
+});
+
+router.get("/rates/topmax", (req, res) => {
+  const stm = `WITH latest_rates AS (
+  SELECT 
+    bank_id, 
+    currency_id,
+    MAX(rate_date) AS latest_rate_date
+  FROM rate
+  GROUP BY bank_id, currency_id
+),
+ranked_rates AS (
+  SELECT
+    r.bank_id,
+    r.id,
+    r.currency_id,
+    lr.latest_rate_date,
+    r.buying_cash,
+    r.selling_cash,
+    r.buying_transaction,
+    r.selling_transaction,
+    (r.selling_cash - r.buying_cash) AS cash_diff,
+    (r.selling_transaction - r.buying_transaction) AS tran_diff,
+    ROW_NUMBER() OVER (PARTITION BY r.currency_id ORDER BY r.buying_cash DESC) AS rn
+  FROM rate r
+  INNER JOIN latest_rates lr 
+    ON r.bank_id = lr.bank_id 
+    AND r.currency_id = lr.currency_id 
+    AND r.rate_date = lr.latest_rate_date
+)
+SELECT 
+  rr.bank_id,
+  b.short_name,
+  b.bank_name,
+  b.address,
+  b.phone_land_line,
+  b.email_address AS bank_email,
+  b.logo,
+  b.color_main,
+  b.color_back,
+  b.color_text,
+  rr.currency_id,
+  rr.latest_rate_date AS rate_date,
+  rr.buying_cash,
+  rr.selling_cash,
+  rr.buying_transaction,
+  rr.selling_transaction,
+  rr.cash_diff,
+  rr.tran_diff,
+  rr.id AS rate_id,
+  c.name AS currency_name,
+  c.description,
+  c.logo AS currency_logo
+FROM ranked_rates rr
+LEFT JOIN bank b ON b.id = rr.bank_id
+LEFT JOIN currency c ON rr.currency_id = c.id
+WHERE rr.rn <= 4
+ORDER BY rr.currency_id, rr.buying_cash DESC;
+`;
   callFunc.DBO(stm, res, "Error Getting Data!!");
 });
 
